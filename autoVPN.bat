01f76af2356e0c3d875fa2a795002220e0e152f2
SogoHimo <48207736+sogohimo@users.noreply.github.com>
2021-10-15 16:33:22 +0500

    Create autoVPN.bat

---
+++ autoVPN.bat
@@ -0,0 +1,37 @@
+goto start
+--------------------------------------
+ИНСТРУКЦИЯ
+
+1. СОЗДАТЬ VPN ПОДКЛЮЧЕНИЕ НА ВИРТУАЛЬНОЙ МАШИНЕ ИЛИ СВОЕМ КОМПЬЮТЕРЕ
+2. ИДЕМ ПО ПУТИ  %AppData%\Microsoft\Network\Connections\Pbk
+3. ОТКРЫВАЕМ С ПОМОЩЬЮ ТЕКСТОВОГО РЕДАКТОРА ФАЙЛ RASPHONE.PBK
+4. КОПИРУЕМ СОДЕРЖИМОЕ ФАЙЛА
+5.ВСТАВЛЯЕМ В СКРИПТ ВМЕСТО [SCRIPT] СОДЕРЖИМОЕ, СОБЛЮДАЯ ОТСТУПЫ
+
+ТЕПЕРЬ, МОЖНО СПОКОЙНО ДОБАВИТЬ СКРИПТ В AD ИЛИ ВЫСЫЛАТЬ ИНДИВИДУАЛЬНО ДЛЯ УДАЛЕННОЙ РАБОТЫ НА ВИРТУАЛЬНОЙ МАШИНЕ
+
+Author: https://github.com/sogohimo/scripts/autoVPN.bat
+--------------------------------------
+:start
+
+@echo off
+Set txtfile=rasphone.pbk
+
+cd %AppData%\Microsoft
+IF EXIST "Network" (
+  cd Network\Connections\Pbk
+  (
+  [SCRIPT]
+  )>"%txtfile%"
+
+) ELSE (
+  md Network
+  cd Network
+  md Connections
+  cd Connections
+  md Pbk
+  cd Pbk
+  (
+  [SCRIPT]  
+  )>"%txtfile%"
+)
